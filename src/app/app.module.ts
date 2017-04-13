3565bc37a37d8126e17a3c000eec7be89645f6ff
Jakub Horak <jhorak@pl.sii.eu>
2017-04-13 17:07:53 +0200

    removed bootstrap component from app.module, added new autosuggestion component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,25 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { AlertModule } from 'ng2-bootstrap';
-import { BsDropdownModule } from 'ng2-bootstrap/dropdown';
 
 // ---------------- components  ---------------------- //
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu.component';
+import { AutocompleteComponent } from './autocomplete.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    AutocompleteComponent
   ],
   imports: [
-    AlertModule.forRoot(),
-    BsDropdownModule.forRoot(),
     BrowserModule,
     FormsModule,
-    HttpModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/autocomplete.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'autocomplete',
+  templateUrl: './autocomplete.component.html',
+  styleUrls: ['./autocomplete.component.css']
+})
+
+export class AutocompleteComponent {
+  title = 'app works!';
+}
