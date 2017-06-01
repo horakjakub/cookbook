7dea3cf5c8366ed19d6e031950f78fd19e624a3f
Jakub Horak <jhorak@pl.sii.eu>
2017-06-01 12:49:49 +0200

    added compodoc and first documentation written by it

---
+++ documentation/js/routes/routes_index.js
@@ -0,0 +1,1 @@
+var ROUTES_INDEX = {"name":"<root>","kind":"module","className":"AppModule","children":[{"kind":"component","component":"SearchPageComponent","path":"search"},{"kind":"component","component":"ContactPageComponent","path":"contact"}]}
